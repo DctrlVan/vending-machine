878b072f24527a8b14ed77c2adbf3e4af21c35cb
DecentralVan <rhodes@protonmail.ch>
2015-08-14 17:11:50 -0700

    Update walletnotify.sh

--- walletnotify.sh
+++ walletnotify.sh
@@ -3,6 +3,6 @@ dir=/home/pi/bbtm
 log=$dir/log/transactions.txt
 echo transaction-id:${1} >> ${log}
 payment_script=$dir/transaction.coffee
-raw_trans=`echo $(bitcoin-cli -regtest decoderawtransaction $(bitcoin-cli -regtest getrawtransaction ${1}))`
+raw_trans=`echo $(bitcoin-cli decoderawtransaction $(bitcoin-cli getrawtransaction ${1}))`
 transaction=`echo $raw_trans|sed  's/ //g'`
 coffee $payment_script $transaction
